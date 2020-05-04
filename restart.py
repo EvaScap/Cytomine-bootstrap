5685c11248bbebc6fa00bf537af0c0777150de4d
Scapellato <eva.scapellato@student.hepl.be>
2020-05-04 18:40:32 +0200

    update of restart.py
    update restart to use compose commands

--- restart.py
+++ restart.py
@@ -1,19 +1,8 @@
-#stop and remove every docker containers 
-#then remove all volumes to initialize and start the docker compose 
 
 import os  
   
 print('STOP CONTAINERS')
-os.system('docker stop $(docker ps -a -q)') 
+os.system('python3 stop.py') 
 
-print('RM CONTAINERS')
-os.system('docker rm $(docker ps -a -q)') 
-
-print('RM VOLUMES')
-os.system('docker volume prune -f')
-
-print('INIT')
-os.system('python3 initialisation.py')
-
-print('START DOCKER COMPOSE')
-os.system('docker-compose up -d')
+print('START CONTAINERS')
+os.system('python3 start.py') 
